65de055bb6d250c01cdec3a50b6e36739138ea49
RocioMaSala <martinrocio16@gmail.com>
2026-02-23 18:10:30 +0100

    nueva version

--- push_swap.c
+++ push_swap.c
@@ -6,12 +6,13 @@
 /*   By: romarti2 <romarti2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 16:59:37 by romarti2          #+#    #+#             */
-/*   Updated: 2026/02/20 14:47:23 by romarti2         ###   ########.fr       */
+/*   Updated: 2026/02/23 18:07:41 by romarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
 
+#include "push_swap.h"
+/*
 void initializer_stack(stack *a)
 {
     s -> front = NULL;
@@ -69,20 +70,143 @@ int pop (stack *delete) // Elimina y devuelve el elemento encima pila
 }
 
 
-void swapa (stack *swapa, int size) // está mal esto
+int swap (stack *swap)
 {
     node *first;
     node *second;
+
+    if (!swap || swap -> size < 2)
+        return(0);
+    first = swap -> front;
+    second = first -> next;// En un nodo si que hay next, pero en una pila no, por eso poner swapa -> next, no tiene sentido 
+    first -> next = second -> next;
+    second -> next = first;
+    swap -> front = second;
+    return (1);
+}
+
+int pushab(stack *pusha, stack *pushb)
+{
+    node *firsta;
+    node *firstb;
+
+    if (!pushb)
+        return (0);
+    firstb = pushb -> front;
+    pushb -> front = firstb -> next; 
+    firstb -> next = pusha-> front;
+    pusha-> front = firstb;
+    return (1);
+       
+}
+
+int rotate(stack *rotate)
+{
+    node *last;
+    node *first;
+    node *second;
+
+    if (!rotate)
+        return (0);
+    first = rotate -> front;
+    second = first -> next;
+    last = rotate -> front;
+    rotate -> front = second;
+    while (last->next != NULL)
+         last = last -> next;
+    last -> next = first;
+    first -> next = NULL;
+    return (1);
+}
+*/
+int reverse_rotate(stack *rr) 
+{
+    node *last;
+    node *prev; // Para gestionar el antepenúltimo
+
+    prev = NULL;
+    last = rr -> front;
+    while (last->next != NULL)
+    {
+        prev = last;
+        last = last -> next;
+    }    
+    prev -> next = NULL;
+    last -> next = rr -> front;
+    rr -> front = last;
+    return (1);
+}
+
+node *new_node(int dato)
+{
+    node *n;
+    
+    n = malloc(sizeof(node));
+    if (!n)
+        return (NULL);
+    n->dato = dato;
+    n->next = NULL;
+    return (n);
+}
+
+void add_back(stack *s, int dato)
+{
+    node *n = new_node(dato);
     node *tmp;
 
-    if (size > 1)
-        return;
-    first = swapa -> front;
-    second = first -> next;
-
-    
-    tmp = second;
-    second = first;
-    first = tmp;
-    free (tmp); 
+    if (!s->front)
+        s->front = n;
+    else
+    {
+        tmp = s->front;
+        while (tmp->next)
+            tmp = tmp->next;
+        tmp->next = n;
+    }
+    s->size++;
 }
+
+void print_stack(stack *s)
+{
+    node *tmp = s->front;
+
+    while (tmp)
+    {
+        printf("%d ", tmp->dato);
+        tmp = tmp->next;
+    }
+    printf("\n");
+}
+// ---------- MAIN ----------
+
+int main(void)
+{
+    stack a;
+    stack b;
+
+    a.front = NULL;
+    a.size = 0;
+    b.front = NULL;
+    b.size = 0;
+
+    add_back(&a, 10);
+    add_back(&a, 20);
+    add_back(&a, 30);
+    add_back(&a, 40);
+    add_back(&b, 50);
+    add_back(&b, 60);
+    add_back(&b, 70);
+
+    printf("Antes:\n");
+    print_stack(&a);
+    print_stack(&b); 
+
+    reverse_rotate(&a);
+
+    printf("Después:\n");
+    print_stack(&a);
+    print_stack(&b);
+
+    return (0);
+}
+
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: romarti2 <romarti2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 16:56:00 by romarti2          #+#    #+#             */
-/*   Updated: 2026/02/19 17:28:08 by romarti2         ###   ########.fr       */
+/*   Updated: 2026/02/23 13:46:40 by romarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,10 @@ typedef struct stack
 	int size;
 }					stack;
 
-/*
-int						ft_lstsize(t_list *lst);
-void 					bubble_sort (t_list *stack);
-int						ft_strlen(char const *str);
-char					*ft_strdup(const char *s);
-t_list				*ft_lstnew(void *content);
-void	ft_lstadd_back(t_list **lst, t_list *new);*/
 
+int reverserotate(stack *rr);
+node *new_node(int dato);
+void add_back(stack *s, int dato);
+void print_stack(stack *s);
 
 #endif
