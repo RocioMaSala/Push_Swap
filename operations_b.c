5558b0b8b48fd6841c1e6c7c68730102962d3d0b
José Ignacio Godino <64109075+Ekon16@users.noreply.github.com>
2026-02-24 16:13:01 +0100

    Implement stack operations sb, rb, and rrb

---
+++ operations_b.c
@@ -0,0 +1,53 @@
+#include "push_swap.h"
+
+void sb(t_stack *b)
+{
+    t_node *first;
+    t_node *second;
+
+    if (!b || b->size < 2)
+        return ;
+    first = b->front;
+    second = first->next;
+    first->next = second->next;
+    if (second->next)
+        second->next->prev = first;
+    second->next = first;
+    second->prev = NULL;
+    first->prev = second;
+    b->front = second;
+    write(1, "sb\n", 3);
+}
+
+
+void rb(t_stack *b)
+{
+    t_node *first;
+
+    if (!b || b->size < 2)
+        return ;
+    first = b->front;
+    b->front = first->next;
+    b->front->prev = NULL;
+    first->next = NULL;
+    first->prev = b->last;
+    b->last->next = first;
+    b->last = first;
+    write(1, "rb\n", 3);
+}
+
+void rrb(t_stack *b)
+{
+    t_node *last;
+
+    if (!b || b->size < 2)
+        return ;
+    last = b->last;
+    b->last = last->prev;
+    b->last->next = NULL;
+    last->prev = NULL;
+    last->next = b->front;
+    b->front->prev = last;
+    b->front = last;
+    write(1, "rrb\n", 4);
+}
