bc427ee101eeb8ad425cfa7b1c1a194b6d784c0d
RocioMaSala <martinrocio16@gmail.com>
2026-03-04 12:44:14 +0100

    norminette op_a.c

--- op_a.c
+++ op_a.c
@@ -1,3 +1,29 @@
-void sa(t_stack *a) { swap_silent(a); write(1, "sa\n", 3); }
-void ra(t_stack *a) { rotate_silent(a); write(1, "ra\n", 3); }
-void rra(t_stack *a) { reverse_rotate_silent(a); write(1, "rra\n", 4); }
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   op_a.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romarti2 <romarti2@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/04 12:43:07 by romarti2          #+#    #+#             */
+/*   Updated: 2026/03/04 12:43:30 by romarti2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	sa(t_stack *a)
+{
+	swap_silent(a);
+	write(1, "sa\n", 3);
+}
+
+void	ra(t_stack *a)
+{
+	rotate_silent(a);
+	write(1, "ra\n", 3);
+}
+
+void	rra(t_stack *a)
+{
+	reverse_rotate_silent(a);
+	write(1, "rra\n", 4);
+}
