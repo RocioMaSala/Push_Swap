7e264822d566f920361f583f8a40676746a5e297
José Ignacio Godino <64109075+Ekon16@users.noreply.github.com>
2026-03-05 17:52:21 +0100

    Add benchmarking to stack operations

--- op_b.c
+++ op_b.c
@@ -16,16 +16,31 @@ void	sb(t_stack *b)
 {
 	swap_silent(b);
 	write(1, "sb\n", 3);
+	if(g_bench.active)
+	{
+		g_bench.sb++;
+		g_bench.total++;
+	}
 }
 
 void	rb(t_stack *b)
 {
 	rotate_silent(b);
 	write(1, "rb\n", 3);
+	if(g_bench.active)
+	{
+		g_bench.rb++;
+		g_bench.total++;
+	}
 }
 
 void	rrb(t_stack *b)
 {
 	reverse_rotate_silent(b);
 	write(1, "rrb\n", 4);
+	if(g_bench.active)
+	{
+		g_bench.rrb++;
+		g_bench.total++;
+	}
 }
